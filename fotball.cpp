4759e1c880d312adee7c57e85c68b61404096d6f
chocoboy420 <mdshohelrana01884537397@gmail.com>
2024-04-28 17:19:20 +0600

    code modify

---
+++ fotball.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (s.find("0000000") < 100 || s.find("1111111") < 100)
+    {
+        cout << "YES" << endl;
+    }
+    else if (s.find("1111111"))
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
